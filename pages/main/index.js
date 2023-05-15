5c3310caacb701cb0a94489616eaee1ad80e28da
eternalclash <suchasucha@naver.com>
2023-05-15 16:00:10 +0900

    Style: 뭐 먹지 페이지 디자인 수정

---
+++ pages/main/index.js
@@ -0,0 +1,25 @@
+const slides = document.getElementsByClassName("fixed-carousel")[0];
+const slide =document.getElementsByClassName('.list-calousel')[0]
+const leftButton = document.getElementsByClassName("left-button")[0];
+const rightButton = document.getElementsByClassName("right-button")[0];
+
+const slideWidth = slides.clientWidth;
+let currentIndex = 0;
+
+function goToSlide(index) {
+  console.log(slideWidth)
+  slides.style.transform = `translateX(-${slideWidth * index}rem`;
+  currentIndex = index;
+}
+
+leftButton.addEventListener("click", () => {
+  if (currentIndex > 0) {
+    goToSlide(currentIndex - 1);
+  }
+});
+
+rightButton.addEventListener("click", () => {
+  if (currentIndex < slides.children.length - 1) {
+    goToSlide(currentIndex + 1);
+  }
+});
