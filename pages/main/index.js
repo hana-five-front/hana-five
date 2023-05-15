7247ca5ee8a8a26848cd90a8afa3384da604b382
eternalclash <suchasucha@naver.com>
2023-05-15 16:46:48 +0900

    Refactor: 리뷰 반영

--- pages/main/index.js
+++ pages/main/index.js
@@ -1,5 +1,5 @@
 const slides = document.getElementsByClassName("fixed-carousel")[0];
-const slide =document.getElementsByClassName('.list-calousel')[0]
+const slide = document.getElementsByClassName(".list-calousel")[0];
 const leftButton = document.getElementsByClassName("left-button")[0];
 const rightButton = document.getElementsByClassName("right-button")[0];
 
@@ -7,7 +7,7 @@ const slideWidth = slides.clientWidth;
 let currentIndex = 0;
 
 function goToSlide(index) {
-  console.log(slideWidth)
+  console.log(slideWidth);
   slides.style.transform = `translateX(-${slideWidth * index}rem`;
   currentIndex = index;
 }
