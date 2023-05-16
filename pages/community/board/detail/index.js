eedb5b8795630d27bf080d1062b46f68aa9ae576
ssangjun <105031887+ssangjun@users.noreply.github.com>
2023-05-16 16:13:14 +0900

    Feat: 커뮤니티 페이지 CRUD 기능 구현 (#16)
    * Feat: 커뮤니티 페이지 구현
    
    * Feat: 커뮤니티 페이지 헤더 추가 및 링크 연결
    
    * Feat: 커뮤니티 페이지 푸터 추가
    
    * Style: 커뮤니티 페이지 a태그, p태그 수정
    
    * Style: 커뮤니티 페이지 메인 컴포넌트 min-height 설정
    
    * Style: 커뮤니티 페이지 p태그 및 디테일 페이지 댓글 css 수정
    
    * Feat: 커뮤니티 페이지 CRUD (로컬스토리지) 구현
    
    * Style: 커뮤니티 페이지 메인 컴포넌트 min-height 수정 및 게시글 상세 context min-height 설정
    
    * Fix: 커뮤니티 페이지 게시글 최신순 정렬 변경
    
    * Feat: 커뮤니티 페이지 게시글 목록 글쓴이 추가
    
    * Fix: 커뮤니티 페이지 콘솔 로그 제거
    
    * Style: prettier 기본 설정 및 적용
    
    * Style: 주석 제거
    
    * Style: prettier Single Quote 설정 및 적용
    
    * Fix: 제목 혹은 내용없이 글 작성 시 내용 삭제되는 현상 수정
    
    * Docs: prettierrc 설정

---
+++ pages/community/board/detail/index.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function () {
+  let detailTitle = document.querySelector('.detailTitle');
+  let detailWriter = document.querySelector('.detailWriter');
+  let detailDate = document.querySelector('.detailDate');
+  let detailContext = document.querySelector('.detailContext');
+
+  let postId = window.location.hash.substring(1).split('#');
+  let posts = JSON.parse(localStorage.getItem('boardPosts')) || [];
+
+  let post = posts.find((post) => post.id == postId);
+
+  if (post) {
+    detailTitle.textContent = post.title;
+    detailWriter.textContent = post.name || '익명';
+    detailDate.textContent = post.date;
+    detailContext.textContent = post.content;
+  } else {
+    alert('해당 게시글을 찾을 수 없습니다.');
+    window.location.href = '../index.html';
+  }
+});
---
+++ pages/community/qna/posting/index.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', function () {
+  let postingButton = document.querySelector('.postingButton');
+  let titleInput = document.querySelector('.postingInputTitle');
+  let nameInput = document.querySelector('.postingInputName');
+  let contentInput = document.querySelector('.postingInputContext');
+
+  postingButton.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    let title = titleInput.value;
+    let name = nameInput.value;
+    if (name === '') {
+      name = '익명';
+    }
+    let content = contentInput.value;
+    let date = new Date().toISOString().split('T')[0];
+
+    if (title === '' || content === '') {
+      alert('제목과 내용을 모두 입력해주세요.');
+    } else {
+      let post = {
+        id: Date.now(),
+        title: titleInput.value,
+        name: name,
+        content: contentInput.value,
+        date: date,
+      };
+
+      let posts = JSON.parse(localStorage.getItem('qnaPosts')) || [];
+
+      posts.push(post);
+      localStorage.setItem('qnaPosts', JSON.stringify(posts));
+
+      window.location.href = '../detail/index.html#' + post.id;
+
+      titleInput.value = '';
+      nameInput.value = '';
+      contentInput.value = '';
+    }
+  });
+});
