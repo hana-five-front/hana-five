0805b4a64d1fb452cd4e9847feb9e906719fcf1d
ssangjun <ssangjun4dev@gmail.com>
2023-05-16 12:59:04 +0900

    Feat: 커뮤니티 페이지 게시글 목록 글쓴이 추가

--- pages/community/board/index.js
+++ pages/community/board/index.js
@@ -3,22 +3,33 @@ document.addEventListener('DOMContentLoaded', function(){
   let posts = (JSON.parse(localStorage.getItem('boardPosts')) || []).reverse();
   boardList.innerHTML = ''
 
-  posts.forEach(function(post, index){
-      let li = document.createElement('li');
-      li.classList.add('boardContent');
-
-      let a = document.createElement('a');
-      a.href = './detail/index.html#' + post.id;
-
-      let pTitle = document.createElement('p');
-      pTitle.textContent = post.title;
-
-      let pDate = document.createElement('p');
-      pDate.textContent = post.date;
-
-      a.appendChild(pTitle);
-      a.appendChild(pDate);
-      li.appendChild(a);
-      boardList.appendChild(li);
-  });
+  for (let i = 0; i < posts.length; i++) {
+    let post = posts[i];
+    let listItem = document.createElement('li');
+    listItem.className = 'boardContent';
+    let link = document.createElement('a');
+    link.href = `./detail/index.html#${post.id}`;
+    
+    let title = document.createElement('p');
+    title.className = 'boardContentTitle';
+    title.textContent = post.title;
+    link.appendChild(title);
+    
+    let infoDiv = document.createElement('div');  // 게시글 정보를 담을 새 div 생성
+    infoDiv.className = 'boardContentInformation';  // 적절한 CSS 클래스 이름 설정
+    
+    let name = document.createElement('p');  // 작성자를 나타내는 새로운 요소 생성
+    name.className = 'boardContentName';  // 적절한 CSS 클래스 이름 설정
+    name.textContent = post.name;  // 작성자 이름 설정
+    infoDiv.appendChild(name);  // 작성자 요소를 정보 div에 추가
+    
+    let date = document.createElement('p');
+    date.className = 'boardContentDate';
+    date.textContent = post.date;
+    infoDiv.appendChild(date);  // 날짜 요소를 정보 div에 추가
+    
+    link.appendChild(infoDiv);  // 정보 div를 링크 요소에 추가
+    listItem.appendChild(link);
+    boardList.appendChild(listItem);
+  }
 });
