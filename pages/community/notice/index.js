38293fce1c80041c93c5df3b9e6f81453df3e519
ssangjun <ssangjun4dev@gmail.com>
2023-05-20 16:11:45 +0900

    Chore: 주석 제거

--- pages/community/notice/index.js
+++ pages/community/notice/index.js
@@ -7,7 +7,7 @@ let currentPage = 1;
 let boardList = document.querySelector('.boardList');
 let pagination = document.querySelector('.boardPage');
 
-let posts = getPosts(postType).reverse(); // 함수 재사용
+let posts = getPosts(postType).reverse();
 
-displayPage(posts, currentPage, boardList); // 함수 재사용
-displayPagination(posts, currentPage, pagination, boardList); // 함수 재사용
+displayPage(posts, currentPage, boardList);
+displayPagination(posts, currentPage, pagination, boardList);
