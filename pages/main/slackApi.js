7f27559ceefaf1aa4a400b954ca805bc3905ebd6
eternalclash <suchasucha@naver.com>
2023-05-20 21:34:42 +0900

    Update: slackApi.js 함수화 및 마크다운 텍스트 변환

--- pages/main/slackApi.js
+++ pages/main/slackApi.js
@@ -1,4 +1,31 @@
-fetch('http://43.200.63.91:3000/slackapi')
+const noticeContainer = document.querySelector('.mainBottomNoticeBottom')
+console.log(noticeContainer)
+
+
+function markDowntoPlainWords (message) {
+   return message.replace(/:[a-zA-Z0-9_]+:|[\*_`~]/g, '');
+}
+
+function makeNotice(title, date) {
+  const outerDiv = document.createElement('div');
+  const innerDiv1 = document.createElement('div');
+  const innerDiv2 = document.createElement('div');
+
+  innerDiv1.textContent = title;
+  innerDiv2.textContent = date;
+
+  outerDiv.appendChild(innerDiv1);
+  outerDiv.appendChild(innerDiv2);
+
+  noticeContainer.appendChild(outerDiv);
+}
+
+function dateToText(date) {
+  return date.split("T")[0]
+}
+
+function getNotice () {
+  fetch('http://43.200.63.91:3000/slackapi')
   .then(function (response) {
     if (response.ok) {
       return response.json();
@@ -6,8 +33,17 @@ fetch('http://43.200.63.91:3000/slackapi')
     throw new Error('Error: ' + response.status);
   })
   .then(function (data) {
-    console.log(data);
+    console.log(data)
+    for (let i = 2; i >= 0; i--) {
+       let {title,date} = data[i]
+       date = dateToText(date)
+       title = markDowntoPlainWords(title)
+       makeNotice(title,date)
+    }
   })
   .catch(function (error) {
     console.log(error);
   });
+}
+
+getNotice()
