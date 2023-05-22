a41b1f52e17b4a29979a9765b8d594e4d15ca907
hyunjoo <79887655+hyunjoolee201910828@users.noreply.github.com>
2023-05-22 21:36:23 +0900

    Fix: config 오류 수정

--- pages/community/board/posting/index.js
+++ pages/community/board/posting/index.js
@@ -2,6 +2,8 @@ import { submitPost } from '../../community.js';
 
 let postType = 'boardPosts';
 let postingButton = document.querySelector('.postingButton');
+const loginBtn = document.querySelector('#login');
+const writeBtn = document.querySelector('.postingSubject');
 
 postingButton.addEventListener('click', function (event) {
   event.preventDefault();
--- scripts/kakaoLogin.js
+++ scripts/kakaoLogin.js
@@ -1,5 +1,16 @@
+import { config } from './apikey.js';
+
+window.Kakao.init(config.apikey);
+
 export const kakaoLoginInit = () => {
   const loginBtn = document.querySelector('#login');
+
+  const sessionUser = sessionStorage.getItem('userName');
+  if (!sessionUser) {
+    loginBtn.innerText = '카카오\n로그인';
+  } else {
+    loginBtn.innerText = '카카오\n로그아웃';
+  }
 
   document.querySelector('#login').addEventListener('click', function () {
     if (loginBtn.innerText == '카카오\n로그인') {
@@ -12,6 +23,12 @@ export const kakaoLoginInit = () => {
               loginBtn.innerText = '카카오\n로그아웃';
               const kakao_account = res.kakao_account;
               alert('login success');
+
+              window.sessionStorage.setItem(
+                'userName',
+                kakao_account.profile.nickname
+              );
+              window.sessionStorage.setItem('userMail', kakao_account.email);
             },
             fail: res => {
               console.error(res);
@@ -23,11 +40,14 @@ export const kakaoLoginInit = () => {
       if (!Kakao.Auth.getAccessToken()) {
         alert('Not logged in.');
         return;
+      } else {
+        Kakao.Auth.logout(function () {
+          window.sessionStorage.setItem('userName', '');
+          window.sessionStorage.setItem('userMail', '');
+          loginBtn.innerText = '카카오\n로그인';
+          alert('logout ok\naccess token -> ' + Kakao.Auth.getAccessToken());
+        });
       }
-      Kakao.Auth.logout(function () {
-        loginBtn.innerText = '카카오\n로그인';
-        alert('logout ok\naccess token -> ' + Kakao.Auth.getAccessToken());
-      });
     }
   });
 };
