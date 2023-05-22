b7a7743d6ba71a69e31b2232f2fc2bfc52407dab
ssangjun <105031887+ssangjun@users.noreply.github.com>
2023-05-22 09:26:26 +0900

    Feat: 커뮤니티 페이지 게시글 목록 페이지네이션 구현 (#33)
    * Feat: 커뮤니티 페이지 게시글 목록 페이지네이션 구현
    
    * Refactor: 커뮤니티 페이지 게시글 상세 함수 모듈화
    
    * Refactor: 커뮤니티 페이지 게시글 등록 함수 모듈화
    
    * Refactor: 커뮤니티 페이지 게시글 목록 함수 모듈화
    
    * Chore: 주석 제거

--- pages/community/notice/detail/index.js
+++ pages/community/notice/detail/index.js
@@ -1,74 +1,10 @@
-document.addEventListener('DOMContentLoaded', function () {
-  let detailTitle = document.querySelector('.detailTitle');
-  let detailWriter = document.querySelector('.detailWriter');
-  let detailDate = document.querySelector('.detailDate');
-  let detailContext = document.querySelector('.detailContext');
-  let detailCommentSubmit = document.querySelector('.detailCommentSubmit');
-  let detailCommentInputWriter = document.querySelector(
-    '.detailCommentInputWriter'
-  );
-  let detailCommentInputContext = document.querySelector(
-    '.detailCommentInputContext'
-  );
-  let detailCommentList = document.querySelector('.detailCommentList');
+import { renderPost, submitComment } from '../../community.js';
 
-  let postId = window.location.hash.substring(1).split('#');
-  let posts = JSON.parse(localStorage.getItem('noticePosts')) || [];
+const postType = 'noticePosts';
+const detailCommentSubmit = document.querySelector('.detailCommentSubmit');
 
-  let post = posts.find(post => post.id == postId);
+renderPost(postType);
 
-  if (post) {
-    detailTitle.textContent = post.title;
-    detailWriter.textContent = post.name || '익명';
-    detailDate.textContent = post.date;
-    detailContext.textContent = post.content;
-    renderComments((post.comments || []).reverse());
-  } else {
-    alert('해당 게시글을 찾을 수 없습니다.');
-    window.location.href = '../index.html';
-  }
-
-  function renderComments(comments) {
-    detailCommentList.innerHTML = '';
-    for (let comment of comments) {
-      let commentElement = document.createElement('div');
-      commentElement.className = 'detailComment';
-
-      let writerElement = document.createElement('p');
-      writerElement.className = 'detailCommentWriter';
-      writerElement.textContent = comment.name || '익명';
-
-      let contextElement = document.createElement('p');
-      contextElement.className = 'detailCommentContext';
-      contextElement.textContent = comment.content;
-
-      commentElement.appendChild(writerElement);
-      commentElement.appendChild(contextElement);
-      detailCommentList.appendChild(commentElement);
-    }
-  }
-
-  detailCommentSubmit.addEventListener('click', function () {
-    let commentName = detailCommentInputWriter.value || '익명';
-    let commentContent = detailCommentInputContext.value;
-    if (commentContent === '') {
-      alert('댓글 내용을 입력해주세요.');
-      return;
-    }
-
-    if (!post.comments) {
-      post.comments = [];
-    }
-
-    post.comments.push({
-      name: commentName,
-      content: commentContent,
-    });
-    localStorage.setItem('noticePosts', JSON.stringify(posts));
-
-    renderComments((post.comments || []).reverse());
-
-    detailCommentInputWriter.value = '';
-    detailCommentInputContext.value = '';
-  });
+detailCommentSubmit.addEventListener('click', function () {
+  submitComment(postType);
 });
