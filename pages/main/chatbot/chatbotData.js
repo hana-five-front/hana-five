f3e853729b345316675c61cb6c1fdd6cae5314fd
juseong <jjs7280@gmail.com>
2023-05-24 15:11:49 +0900

    Update: 챗봇 기능 수정
    * Modify: 챗봇 응답 내용 수정
    
    * Style: 챗봇 로고 수정

--- pages/main/chatbot/chatbotData.js
+++ pages/main/chatbot/chatbotData.js
@@ -10,6 +10,9 @@ export const FAQ_LIST = [
   { resId: 6, reqGroup: 2, nextReqGroup: 0, contents: '교육 특전 문의' },
   { resId: 7, reqGroup: 2, nextReqGroup: 0, contents: '강사님 정보' },
   { resId: 8, reqGroup: 2, nextReqGroup: 0, contents: '시간표 문의' },
+  { resId: 9, reqGroup: 0, nextReqGroup: 0, contents: '처음으로 돌아가기' },
+  { resId: 9, reqGroup: 1, nextReqGroup: 0, contents: '처음으로 돌아가기' },
+  { resId: 9, reqGroup: 2, nextReqGroup: 0, contents: '처음으로 돌아가기' },
 ];
 
 export const ANSWER_LIST = [
@@ -17,69 +20,78 @@ export const ANSWER_LIST = [
     id: 0,
     resId: 0,
     type: 'req',
-    contents:
-      '안녕하세요, 디지털 하나로 문의하기 화면입니다. 궁금한 사항을 클릭해주세요..! ',
+    contents: `디지털 하나로는 금융권 최초의 KDT 교육으로, 데이터 분석 및 기획, 금융서비스 개발 파트로 나뉘어 금융권 디지털/ICT 인재를 키우는데 특화된 교육입니다.
+      
+      고용노동부가 지원하고 하나은행이 대한상공회의소 및 금융권 디지털 전문교육업체와 협력하여 훈련과정을 설계/운영하는 금융권 맞춤 디지털 실무인재 양성 프로그램입니다(훈련비 무료).
+      
+      또한, 하나은행만의 인재 양성 노하우를 바탕으로 취업 준비생이 디지털 인재로 성장하고 나아가 금융권에 취업하는데 방향(路: 길 로)을 제시하고자 합니다.`,
   },
   {
     id: 1,
     resId: 1,
     type: 'req',
-    contents: `디지털 하나로는 금융권 최초의 KDT 교육으로, 
-      데이터 분석 및 기획, 금융서비스 개발 파트로 나뉘어 금융권 디지털/ICT 인재를 키우는데 특화된 교육입니다.`,
+    contents: `디지털 하나로는 은행권 디지털/ICT 직무에 관심있는 모든 분들을 대상으로 합니다. 
+
+    다만 교육 수료 후 입사가 가능한 경우로 입교를 제한하고 있습니다.`,
   },
   {
     id: 2,
     resId: 2,
     type: 'req',
-    contents: `디지털 하나로는 은행권 디지털/ICT 직무에 관심있는 모든 분들을 대상으로 합니다. 
-        다만 교육 수료 후 입사가 가능한 경우로 입교를 제한하고 있습니다.`,
+    contents: `커리큘럼은 금융의 이해, 서비스 개발의 요소, Full Stack 개발(Frontend 및 Backend 개발)로 이루어집니다.
+    
+    자세한 내용은 교육 > 교육 과정 페이지에서 확인하실 수 있습니다. `,
   },
   {
     id: 3,
     resId: 3,
     type: 'req',
-    contents: '커리큘럼은 커리큘럼 페이지로 가시죠..!',
+    contents: '교육 생활은 좋은 환경의 알파코 성수 캠퍼스에서 진행됩니다.',
   },
   {
     id: 4,
     resId: 4,
     type: 'req',
-    contents: '교육 생활은 아주 좋아요..!',
+    contents: `Frontend는 HTML/CSS/Javascript, 
+    Backend는 JAVA/SPRING, ORACLE DB를 배우게 되며, 풀스택 과정으로 진행됩니다.`,
   },
   {
     id: 5,
     resId: 5,
     type: 'req',
-    contents: 'HTML/CSS/JS, JAVA/SPRING, ORACLE DB',
+    contents: `교육은 5월부터 10월까지 6개월 동안 이루어 집니다. 
+      
+      긴 과정이지만 꼭 알찬 시간이 되시길 바랍니다...!!!`,
   },
   {
     id: 6,
     resId: 6,
     type: 'req',
-    contents:
-      '교육기간은 5월 부터 10월입니다. 길지만 알찬 시간이 되길 꼭 바랍니다...!!!',
+    contents: `수료생의 70%를 우수 및 최우수 수료생으로 선발하여 하나금융그룹 서류전형을 면제해드립니다.
+
+        최우수 수료생의 경우 필기 전형 및 코딩테스트 전형을 제외하고, 바로 면접을 볼 수 있습니다.`,
   },
   {
     id: 7,
     resId: 7,
     type: 'req',
-    contents: `수료생의 70%를 우수 및 최우수 수료생으로 선발하여 하나금융그룹 서류전형을 면제해드립니다. 
-        최우수 수료생의 경우 필기 전형 및 코테 전형을 제외하고, 바로 면접을 볼수있습니다.
-      `,
+    contents: `강사님은 정현희 강사님입니다.
+    
+    삼성 멀티캠퍼스, 대한상공회의소 전임 교수로 강의하신 적이 있으며 "알기 쉬운 파이썬/SQL 코딩하기" 책을 집필하신 이력이 있습니다.`,
   },
   {
     id: 8,
     resId: 8,
     type: 'req',
-    contents: `강사님은 정현희 강사님입니다..!
-      `,
+    contents: `자세한 시간표는 교육 > 교육 과정 페이지에서 확인하실 수 있습니다. 
+    `,
   },
   {
     id: 9,
     resId: 9,
     type: 'req',
-    contents: `시간표는 시간표 페이지로 가시죠..! 
-      `,
+    contents:
+      '안녕하세요, 디지털 하나로 문의하기 화면입니다. 궁금한 사항을 클릭해주세요..!',
   },
 ];
 
