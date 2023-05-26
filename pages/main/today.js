84be000db0228df56ce313db76321b5ee3759e73
eternalclash <suchasucha@naver.com>
2023-05-26 23:23:24 +0900

    Style: 헤더 수정 및 레이아웃 수정 및 시간표 수정

--- pages/main/today.js
+++ pages/main/today.js
@@ -3,22 +3,23 @@ const today = new Date(Date.now())
   .split('.')
   .map(e => Number(e));
 const todayTable = document.querySelector('.todayTableContent');
+const todayTableTitle = document.querySelector('.todayTableTitle')
 const planData = async () => {
   const planResponse = await fetch('../curriculum/calendar/data/data.json');
   const data = await planResponse.json();
+  const todayText = document.createElement('div')
+  todayText.innerText = `${today[0]}년 ${today[1]}월 ${today[2]}일`
+  todayTableTitle.appendChild(todayText)
+  console.log(today)
   let todayData = data[`${today[1]}월`][today[2]];
   if (todayData) {
     todayData.forEach(timeSchedule => {
       let timeText = timeSchedule.split(' ')[0][1];
-      let classText = timeSchedule.split(' ').slice(1).join('');
-      const timeDiv = document.createElement('div');
-      const classDiv = document.createElement('div');
-      timeDiv.style = 'color: #008485; ';
-      timeDiv.innerText = `[${timeText}H]`;
-      classDiv.innerText = classText;
+      let classText = "- " + timeSchedule.split(' ').slice(1).join('');
+      const classDiv = document.createElement('div');;
+      classDiv.innerText = classText+" "+`(${timeText}H)`;
 
       todayTable.appendChild(classDiv);
-      todayTable.appendChild(timeDiv);
     });
   }
 };
