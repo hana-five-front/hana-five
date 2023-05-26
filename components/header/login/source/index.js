65a4897d81c6fbb2c737819ecab1de82d3d7b268
ChaedongIm <chaedong.im.dev@gmail.com>
2023-05-26 20:38:11 +0900

    Update: 카카오 로그인 -> 로그인 으로 문구 수정

--- components/header/login/source/index.js
+++ components/header/login/source/index.js
@@ -1,14 +1,14 @@
 var loginbtn = document.getElementById('login');
 
 document.getElementById('login').onclick = function () {
-  if (loginbtn.innerText == '카카오\n로그인') {
+  if (loginbtn.innerText == '로그인') {
     window.Kakao.Auth.login({
       scope: 'profile_nickname, account_email',
       success: authObj => {
         window.Kakao.API.request({
           url: '/v2/user/me',
           success: res => {
-            loginbtn.innerText = '카카오\n로그아웃';
+            loginbtn.innerText = '로그아웃';
             const kakao_account = res.kakao_account;
             alert('login success');
           },
@@ -18,13 +18,13 @@ document.getElementById('login').onclick = function () {
         });
       },
     });
-  } else if (loginbtn.innerText == '카카오\n로그아웃') {
+  } else if (loginbtn.innerText == '로그아웃') {
     if (!Kakao.Auth.getAccessToken()) {
       alert('Not logged in.');
       return;
     }
     Kakao.Auth.logout(function () {
-      loginbtn.innerText = '카카오\n로그인';
+      loginbtn.innerText = '로그인';
       alert('logout ok\naccess token -> ' + Kakao.Auth.getAccessToken());
     });
   }
