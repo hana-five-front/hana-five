d4c01c281c8552d2f48aa015d20837a43fedccbd
juseong <jjs7280@gmail.com>
2023-05-26 12:49:20 +0900

    Modify: console.log 제거

--- pages/main/chatbot/chatbotSlackApi.js
+++ pages/main/chatbot/chatbotSlackApi.js
@@ -7,6 +7,5 @@ export const sendQnaToSlack = (userName, content) => {
     body: JSON.stringify({ text: `${userName}\n\n${content}` }),
   })
     .then(response => response.json())
-    .then(data => console.log('Message sent:', data))
     .catch(error => console.error('Error sending message:', error));
 };
