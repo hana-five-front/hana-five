ae7c735a0a5207a17ab3427965975655864c27cd
hyunjoo <79887655+hyunjoolee201910828@users.noreply.github.com>
2023-05-30 16:49:17 +0900

    Update: interval 함수화

--- pages/main/index.js
+++ pages/main/index.js
@@ -13,6 +13,8 @@ let slidesLength = slides.children.length;
 let clonedFirst = slideList.firstElementChild.cloneNode(true);
 let clonedLast = slideList.lastElementChild.cloneNode(true);
 
+let time = 5000;
+
 slideList.appendChild(clonedFirst);
 slideList.insertBefore(clonedLast, slideList.firstElementChild);
 
@@ -21,47 +23,6 @@ function goToSlide(index) {
   slides.style.transform = 'translateX(' + -slideWidth * index + 'px)';
   currentIndex = index;
 }
-
-leftButton.addEventListener('click', () => {
-  if (currentIndex > 0) {
-    number = parseInt(number) > 1 ? parseInt(number) - 1 : parseInt(4);
-    goToSlide(currentIndex - 1);
-  } else if (currentIndex == 0) {
-    slides.style.transition = '0ms';
-    slides.style.transform = 'translateX(' + -slideWidth * slidesLength + 'px)';
-    number = 4;
-    resultElement.innerText = number;
-    currentIndex = slidesLength + 1;
-    setTimeout(() => {
-      slides.style.transition = 'transform 0.3s ease';
-      goToSlide(3);
-    }, 0);
-  }
-});
-
-rightButton.addEventListener('click', () => {
-  if (currentIndex <= slidesLength + 2) {
-    slides.style.transition = 'transform 0.3s ease';
-    number = parseInt(number) < 4 ? parseInt(number) + 1 : parseInt(1);
-    goToSlide(currentIndex + 1);
-  }
-  if (currentIndex == slidesLength + 2) {
-    setTimeout(
-      function () {
-        slides.style.transition = 'transform 0.3s ease';
-        slides.style.transform = 'translateX(' + -slideWidth + 'px)';
-        number = 2;
-        resultElement.innerText = number;
-      },
-      [300]
-    );
-    slides.style.transition = '0ms';
-    goToSlide(1);
-    number = 2;
-    resultElement.innerText = number;
-    currentIndex = 1;
-  }
-});
 
 const autoSlideHandler = () => {
   if (currentIndex <= slidesLength + 2) {
@@ -86,16 +47,62 @@ const autoSlideHandler = () => {
     currentIndex = 1;
   }
 };
+let slideInterval = setInterval(autoSlideHandler, [time]);
 
-let slideInterval = setInterval(autoSlideHandler, [5000]);
+leftButton.addEventListener('click', () => {
+  if (currentIndex > 0) {
+    number = parseInt(number) > 1 ? parseInt(number) - 1 : parseInt(4);
+    goToSlide(currentIndex - 1);
+  } else if (currentIndex == 0) {
+    slides.style.transition = '0ms';
+    slides.style.transform = 'translateX(' + -slideWidth * slidesLength + 'px)';
+    number = 4;
+    resultElement.innerText = number;
+    currentIndex = slidesLength + 1;
+    setTimeout(() => {
+      slides.style.transition = 'transform 0.3s ease';
+      goToSlide(3);
+    }, 0);
+  }
+  resetInterval();
+});
+
+rightButton.addEventListener('click', () => {
+  if (currentIndex <= slidesLength + 2) {
+    slides.style.transition = 'transform 0.3s ease';
+    number = parseInt(number) < 4 ? parseInt(number) + 1 : parseInt(1);
+    goToSlide(currentIndex + 1);
+  }
+  if (currentIndex == slidesLength + 2) {
+    setTimeout(
+      function () {
+        slides.style.transition = 'transform 0.3s ease';
+        slides.style.transform = 'translateX(' + -slideWidth + 'px)';
+        number = 2;
+        resultElement.innerText = number;
+      },
+      [300]
+    );
+    slides.style.transition = '0ms';
+    goToSlide(1);
+    number = 2;
+    resultElement.innerText = number;
+    currentIndex = 1;
+  }
+  resetInterval();
+});
 
 const observeScreenResizeEvent = () => {
   window.addEventListener('resize', () => {
     slideWidth = slides.clientWidth;
-    clearInterval(slideInterval);
-    slideInterval = setInterval(autoSlideHandler, 5000);
+    resetInterval();
     slides.style.transform = 'translateX(' + -slideWidth + 'px)';
   });
 };
 
 observeScreenResizeEvent();
+
+const resetInterval = () => {
+  clearInterval(slideInterval);
+  slideInterval = setInterval(autoSlideHandler, time);
+};
