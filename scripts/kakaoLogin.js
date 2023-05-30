006a2fd31ae9ff213c96f0fdee7f7dedf262c0d5
eternalclash <77526745+eternalclash@users.noreply.github.com>
2023-05-30 18:11:38 +0900

    Update kakaoLogin.js

--- scripts/kakaoLogin.js
+++ scripts/kakaoLogin.js
@@ -1,4 +1,4 @@
-fetch('http://localhost:3000/header')
+fetch('https://server-eternalclash.koyeb.app/header')
     .then(function (response) {
       if (response.ok) {
         return response.json();
