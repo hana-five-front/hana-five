37e68111c728d3e346f240fbea916d1259ab032b
ChaedongIm <chaedong.im.dev@gmail.com>
2023-06-02 23:46:26 +0900

    Update: 캐러셀 하이트 최신화 로직 추가

--- pages/main/index.js
+++ pages/main/index.js
@@ -135,8 +135,7 @@ const setLogoToWhite = $target => {
   $target.classList.remove('transparentLogo');
 };
 
-const carouselHeight = document.querySelector('.carousel').scrollHeight - 64;
-
+let carouselHeight = document.querySelector('.carousel').scrollHeight - 64;
 const setLogoByScreenWidth = () => {
   if (window.innerWidth > 1210 || window.scrollY > carouselHeight) {
     setHeaderWhite();
@@ -147,6 +146,7 @@ const setLogoByScreenWidth = () => {
 
 const observeScreenResizeEvent = () => {
   window.addEventListener('resize', () => {
+    carouselHeight = document.querySelector('.carousel').scrollHeight - 64;
     resetSlideWidth();
     setLogoByScreenWidth();
   });
